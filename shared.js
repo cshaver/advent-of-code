d6736d61de6a464a8b70a26b54adb6441ccd25a0
Cristina Shaver <cristina.shaver@gmail.com>
2015-12-03 12:57:29 -0800

    add sort of an interface

---
+++ shared.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// attatches function to button/result pair
+function hookUpButton(fn, day, part) {
+  var input = document.getElementById('input-day'+day);
+  var button = document.getElementById('button-day'+day+'-part'+part);
+  var result = document.getElementById('result-day'+day+'-part'+part);
+
+  var getResult = function() {
+    // run the function on the text in the input field
+    // and put the result in the result input field
+    result.value = fn(input.value);
+  };
+
+  // attach listener
+  button.addEventListener('click', getResult);
+
+  // run it once
+  getResult();
+}
